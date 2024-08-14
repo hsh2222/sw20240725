0090d6accee1652cd237c366adf13850265be7a2
hsh2222 <tkdgnsgks566245@naver.com>
2024-08-14 10:32:24 +0900

    Day15

---
+++ 2024-08-14/album/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // App 컴포넌트를 import
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
